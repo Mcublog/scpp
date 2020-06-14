6948d57e54354e7cce3e36e11fa3d532b8ed9c23
Viacheslav <viacheslav@mcublog.xyz>
2020-06-14 12:34:08 +0300

    Added proto_cmd_list.h template;

---
+++ proto_cmd_list.h
@@ -0,0 +1,12 @@
+#ifndef CONSOLE_CMD_LIST_H
+#define CONSOLE_CMD_LIST_H
+
+typedef enum
+{
+    EMPTY = 0,
+    Get_Name,
+    //... add you code id ...//
+    ID_LAST_CMD
+} cmd_id_t; //command table
+
+#endif //CONSOLE_CMD_LIST_H
