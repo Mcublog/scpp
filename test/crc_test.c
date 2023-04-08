514f5dedbcafd92aca2f83bd67e4717837cf33ca
Viacheslav <viacheslav@mcublog.xyz>
2023-04-08 15:23:49 +0300

    added crc checking in test

--- test/crc_test.c
+++ test/crc_test.c
@@ -15,7 +15,13 @@
 
 int main(void)
 {
+    const uint32_t CORRECT_CRC32 = 0xb14257cc;
     const uint8_t buffer[] = {0x11, 0x22, 0x33, 0x44};
     uint32_t crc = xcrc32(buffer, sizeof(buffer), 0xFFFFFFFF);
-    printf("crc: 0x%x\n", crc);
+    if (crc != CORRECT_CRC32)
+    {
+        printf("crc != CORRECT_CRC32: 0x%x != 0x%x\n", crc, CORRECT_CRC32);
+        exit(1);
+    }
+    printf("crc: 0x%x is correct\n", crc);
 }
