5087a983b4b439e66332c7f0e796645a4dfb70d3
Viacheslav <viacheslav@mcublog.xyz>
2023-04-08 20:00:16 +0300

    added new test data

--- console/logger_data_structures.py
+++ console/logger_data_structures.py
@@ -1,7 +1,7 @@
 import ctypes
 from ctypes import Structure, c_int16, c_uint16, c_uint32
 
-from console.test_data import TEST_FLASH_DATA_ACK_ACC
+from console.test_data import TEST_FLASH_DATA_ACK_ACC_MORE_SIZE
 
 
 # yapf: disable
@@ -10,7 +10,8 @@ class time_stamp_t(Structure):
         ('time',    c_uint32),
         ('t_ms',    c_uint16),
         ('day',     c_uint16),
-        ('date',    c_uint16)]
+        ('date',    c_uint16)
+        ]
 
 
 class acc_header_t(Structure):
@@ -19,12 +20,14 @@ class acc_header_t(Structure):
         ('freq', c_uint32),
         ('rezolution', c_uint32),
         ('max_size', c_uint32),
-        ('acc_i', c_uint32)]
+        ('acc_i', c_uint32)
+        ]
 
 class acc_report_t(Structure):
     _fields_ = [
         ('h', acc_header_t),
-        ('acc_buf', c_uint32)]
+        ('acc_buf', c_uint32)
+        ]
 
 class mem_mngr_h_t(Structure):
     _fields_ = [
@@ -41,29 +44,34 @@ class AxesRaw_t(Structure):
     ('AXIS_X', c_int16),
     ('AXIS_Y', c_int16),
     ('AXIS_Z', c_int16)]
+
+    def __str__(self):
+        return f'x: {self.AXIS_X:05d} y: {self.AXIS_Y:05d} z: {self.AXIS_Z:05d}'
 # yapf: enable
 
-def extract_binary_data(buffer:bytes) -> bytes:
+
+def extract_binary_data(buffer: bytes) -> bytes:
     PREFIX_SIZE = len(b'[(\x08\x00\x00][GET_FLASH_DATA][bin][1][')
     END_SIZE = -9
     if len(buffer) < PREFIX_SIZE:
         return b''
     return buffer[PREFIX_SIZE:END_SIZE]
 
+
 def main():
     # print(TEST_FLASH_DATA_ACK)
-    data = extract_binary_data(TEST_FLASH_DATA_ACK_ACC)
+    data = extract_binary_data(TEST_FLASH_DATA_ACK_ACC_MORE_SIZE)
     # print(data)
     header = data[:ctypes.sizeof(mem_mngr_h_t)]
     h = mem_mngr_h_t.from_buffer_copy(header)
     report_buffer = data[ctypes.sizeof(mem_mngr_h_t):]
-    print(h)
+    # print(h)
+    print(f'type: {h.type} part: {h.part} dsize: {h.dsize} crc32: {h.crc32:#x}')
     # print(report)
     for i in range(0, h.dsize * ctypes.sizeof(AxesRaw_t), ctypes.sizeof(AxesRaw_t)):
         report = AxesRaw_t.from_buffer_copy(report_buffer, i)
-        print(
-            f'nbr: {int(i/ctypes.sizeof(AxesRaw_t)):04d} x: {report.AXIS_X:05d} y: {report.AXIS_Y:05d} z: {report.AXIS_Z:05d}'
-        )
+        print(f'nbr: {int(i/ctypes.sizeof(AxesRaw_t) + 1):04d} {report}')
+
 
 if __name__ == '__main__':
     main()
